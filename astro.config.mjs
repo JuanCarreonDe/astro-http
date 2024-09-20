89329bd9255aa6856bb54b443daca0bfd6e7b9ae
juancarreonde <juancarreon@gmail.com>
2024-09-20 12:04:57 -0600

    add cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,10 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-
-  // output: "hybrid",
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
   integrations: [mdx(), sitemap()],
-
-  output: "server",
-  adapter: cloudflare(),
 });
