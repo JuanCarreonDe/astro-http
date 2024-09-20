8a4027d2a9e48ee2b1ebc92a62eb0b6f11b3e034
juancarreonde <juancarreon@gmail.com>
2024-09-20 12:19:15 -0600

    install again cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: cloudflare(),
 });
